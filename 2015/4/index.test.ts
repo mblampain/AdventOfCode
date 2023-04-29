cdb3bf938147cdb0c6b0aab38513ae6f1927b564
mblampain <maxime.blampain@amalia.io>
2023-04-29 15:21:40 +0200

    2015-4

---
+++ 2015/4/index.test.ts
@@ -0,0 +1,17 @@
+import { solve } from './index';
+it.each`
+  input       | result
+  ${"abcdef"}  | ${609043}
+  ${"pqrstuv"}  | ${1048970}
+`("$input should return $result", ({ input, result }) => {
+    const out = solve(input)[0];
+    expect(out).toBe(result);
+});
+
+it.skip.each`
+  input       | result
+  ${"0"}  | ${0}
+`("$input should return $result", ({ input, result }) => {
+    const out = solve(input)[1];
+    expect(out).toBe(result);
+});
---
+++ 2015/4/index.ts
@@ -0,0 +1,28 @@
+import { readFileSync } from 'node:fs';
+import md5 from 'md5';
+
+const startsWithXZeroes = (hash:string, x: number) => [...hash.slice(0,x)].every(c => c === '0');
+
+export const solve =(input: string): [number, number] => {
+    let hash: string = '';
+    let fiveZ = 0;
+    do {
+        fiveZ++;
+        hash = md5(input.concat(fiveZ.toString()));
+    } while(!startsWithXZeroes(hash, 5))
+    let sixZ = 0;
+    do {
+        sixZ++;
+        hash = md5(input.concat(sixZ.toString()));
+    } while(!startsWithXZeroes(hash, 6))
+    return [fiveZ, sixZ];
+}
+
+const exec = () => {
+    const input: string = readFileSync('./input.txt', 'utf-8');
+    const result = solve(input);
+    console.log('The final result is : ');
+    console.log(result);
+}
+
+exec();
